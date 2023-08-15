8be3477b8443e25a92e841081f112e908c117c5b
cecile hirschauer <cecile.h.75@gmail.com>
2023-08-15 13:17:23 +0200

    first CRUD

--- database.js
+++ database.js
@@ -1,16 +1,31 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: procees.env.MYSQL_HOST,
-    user: procees.env.MYSQL_USER,
-    password: procees.env.MYSQL_PASSWORD,
-    database: procees.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 }).promise();
 
-const getNotes = async () => {
+export const getNotes = async () => {
     const [rows] = await pool.query('SELECT * FROM notes');
     return rows;
-}
+};
+
+export const getNote = async (id) => {
+    const [rows] = await pool.query('SELECT * FROM notes WHERE id = ?', [id]);
+    return rows[0];
+};
+
+export const createNote = async (title, content) => {
+    const [result] = await pool.query('INSERT INTO notes (title, contents) VALUES (?, ?)', [title, content]);
+    const id = result.insertId;
+    return getNote(id);
+};
 
 const notes = await getNotes();
-console.log(notes);
+const note = await getNote(2);
+const newNote = await createNote('Brain New Note', 'This is another new note');
+console.log(newNote);
